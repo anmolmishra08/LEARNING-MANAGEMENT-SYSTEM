5264ae2db3023f04ad108d5e431eef41695f8f3f
Anmol Mishra <anmol14108@gmail.com>
2026-03-03 19:54:33 +0530

    Updated axios to use VITE_BACKEND_URL

--- Client/src/Helpers/axiosinstance.js
+++ Client/src/Helpers/axiosinstance.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api/v1`;
 
-const axiosInstance = axios.create();
-
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
 
 export default axiosInstance;
